ccf628a548ba71e8d6001282e8d8b2a031bd9198
Omerovic <ahmed.omerovic@etu.univ-savoie.fr>
2023-03-27 12:44:49 +0200

    pas prey

--- twittos/server.js
+++ twittos/server.js
@@ -1,29 +1,30 @@
 const express = require('express');
 const mysql = require('mysql2/promise');
-
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
-const config = {
+// MySQL Connection Pool
+const pool = mysql.createPool({
   host: 'localhost',
   user: 'mon_user',
   password: 'mon_mot_de_passe',
   database: 'ma_base_de_donnees'
-};
+});
 
+// GET route to retrieve data from the database
 app.get('/data', async (req, res) => {
   try {
-    const connection = await mysql.createConnection(config);
-    const [rows] = await connection.execute('SELECT * FROM tweet');
+    const connection = await pool.getConnection();
+    const [rows, fields] = await connection.execute('SELECT * FROM tweet');
+    connection.release();
     res.json(rows);
-    connection.end();
   } catch (error) {
-    console.log(error);
+    console.error(error);
     res.status(500).send('Error retrieving data from database');
   }
 });
 
+// Start the server
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
 
 
