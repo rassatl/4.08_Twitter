3ff8c70bc6d113b90df2f659a97e3925cd9a343d
Omerovic <ahmed.omerovic@etu.univ-savoie.fr>
2023-03-30 10:08:15 +0200

    Emoji

--- twittos/src/components/HomeComponent.vue
+++ twittos/src/components/HomeComponent.vue
@@ -1,9 +1,10 @@
 <script setup>
+
 import WelcomeItem from './WelcomeItem.vue'
 import TweetComponent from './TweetComponent.vue'
-
 import { ref } from 'vue';
 import axios from 'axios'
+
 //idProfil,msg, obj, reply, retweet, lik, view 
 const tweet = ref({
   idProfil: 1,
@@ -24,6 +25,24 @@ function callFunctionInsertTweet() {
             console.error(error);
         });
 }
+
+function showEmojis() {
+  const emojis = ['😀', '😂', '😍', '👍', '❤️']
+  const emojiPopup = document.createElement('div')
+  emojiPopup.classList.add('emoji-popup')
+  emojis.forEach(emoji => {
+    const emojiBtn = document.createElement('button')
+    emojiBtn.innerText = emoji
+    emojiBtn.addEventListener('click', () => {
+      this.textInput += emoji
+      emojiPopup.remove()
+    })
+    emojiPopup.appendChild(emojiBtn)
+  })
+  document.body.appendChild(emojiPopup)
+}
+
+
 </script>
 
 <template>
@@ -72,7 +91,7 @@ function callFunctionInsertTweet() {
             </svg>
           </div>
           <div id="bgIcons">
-            <svg viewBox="0 0 24 24" aria-hidden="true" class="icon">
+            <svg @click="showEmojis" viewBox="0 0 24 24" aria-hidden="true" class="icon">
               <g>
                 <path
                   d="M8 9.5C8 8.119 8.672 7 9.5 7S11 8.119 11 9.5 10.328 12 9.5 12 8 10.881 8 9.5zm6.5 2.5c.828 0 1.5-1.119 1.5-2.5S15.328 7 14.5 7 13 8.119 13 9.5s.672 2.5 1.5 2.5zM12 16c-2.224 0-3.021-2.227-3.051-2.316l-1.897.633c.05.15 1.271 3.684 4.949 3.684s4.898-3.533 4.949-3.684l-1.896-.638c-.033.095-.83 2.322-3.053 2.322zm10.25-4.001c0 5.652-4.598 10.25-10.25 10.25S1.75 17.652 1.75 12 6.348 1.75 12 1.75 22.25 6.348 22.25 12zm-2 0c0-4.549-3.701-8.25-8.25-8.25S3.75 7.451 3.75 12s3.701 8.25 8.25 8.25 8.25-3.701 8.25-8.25z"
@@ -233,5 +252,23 @@ function callFunctionInsertTweet() {
   resize: none;
   border: none;
   left: 1rem;
+}
+
+.emoji-popup {
+  position: absolute;
+  top: 100%;
+  left: 0;
+  display: flex;
+  flex-wrap: wrap;
+  background-color: white;
+  border: 1px solid gray;
+  padding: 10px;
+}
+
+.emoji-popup button {
+  background-color: transparent;
+  border: none;
+  font-size: 24px;
+  cursor: pointer;
 }
 </style>
